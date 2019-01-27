78e3da7b07d78f6985ece5a89244c9c3b828661a
Jonah Simpson <jonahs99@gmail.com>
2019-01-26 23:06:36 -0500

    Add pitstops but still simple scaling

--- src/model.py
+++ src/model.py
@@ -13,87 +13,187 @@ props['tire'] =			[500,	750,	1000,	1250,	1500]
 props['handling'] =		[9,		12,		15,		18,		21	]
 
 def optimize(config, track):
+	config = {prop: props[prop][config[i]-1] for i, prop in
+		enumerate(['tire', 'gas', 'handling', 'speed', 'acceleration', 'breaking'])}
+	'''
 	TIRE = props['tire'][config[0]-1]
 	GAS = props['gas'][config[1]-1]
 	HANDLE = props['handling'][config[2]-1] 
 	TOP = props['speed'][config[3]-1]
 	ACC = props['acceleration'][config[4]-1] 
 	BRK = props['breaking'][config[5]-1] 
-
-	#radius = np.loadtxt(TRACK_FILE, skiprows=1)
-	radius = track
+	'''
 
 	# Convert to max speeds
 
-	v_max = np.sqrt(np.abs(radius) * HANDLE / 1e6)
-	v_max = np.minimum(v_max, TOP)
-	v_max[radius == -1] = TOP
-
+	v_max = np.sqrt(np.abs(track) * config['handling'] / 1e6)
+	v_max = np.minimum(v_max, config['speed'])
+	v_max[track == -1] = config['speed']
 	v_max[1:] = np.minimum(v_max[:-1], v_max[1:])
 
 	v_max[0] = 0
 
-	# Optimal travel
+	# Prep
 
-	speed = np.zeros(v_max.shape)
+	unconstr_v = v_max.copy()
+	unconstrain(config, unconstr_v) 
 
-	for i in range(1, len(speed)):
-		speed[i] = min(math.sqrt( speed[i-1] ** 2 + 2 * ACC ), v_max[i])
+	# Try pitstops
 
-	for i in range(len(speed) - 2, -1, -1):
-		speed[i] = min(math.sqrt( speed[i+1] ** 2 - 2 * BRK ), speed[i])
+	stats = []
+	for n_pits in range(10):
+		acc, pit_locs = opt_with_pits(config, unconstr_v, n_pits)
+		acc *= (1-EPS)
 
-	acc = (speed[1:]**2 - speed[:-1]**2) / 2
-	acc = np.append(acc, [0])
+		v = np.zeros(acc.shape)
+		for i, a in enumerate(acc[:-1]):
+			discr = v[i]**2 + 2 * a
+			v[i+1] = math.sqrt(discr) if discr > 0 else 0
 
-	# Costs
+		time = 0
+		for i, a in enumerate(acc[:-1]):
+			time += ((v[i+1] - v[i]) / a) if abs(a) > EPS else 1 / v[i]
 
-	gas = np.cumsum((0.1*np.maximum(acc, 0)**2))
-	tire = np.cumsum((0.1*np.minimum(acc, 0)**2))
+		time += 30 * n_pits
+	
+		#for i, s in enumerate(v[490:510]):
+		#	print(i+490, s)
+	
+		gas = np.sum((0.1*np.maximum(acc, 0)**2))
+		tire = np.sum((0.1*np.minimum(acc, 0)**2))
 
-	# Scale to save gas money
+		instr = np.zeros((len(acc), 2))
+		instr[:,0] = acc
+		instr[:,1][pit_locs] = 1
 
-	c_gas = math.sqrt(GAS / gas[-1])
-	c_tire = math.sqrt(TIRE / tire[-1])
-	c = min(c_gas, c_tire, 1)
+		if len(stats) and time >= stats[-1][1]:
+			break
+
+		stats.append( (instr, time, gas, tire) )
+
+	return stats[-1]
+
+def unconstrain(config, v):
+	for i in range(1, len(v)):
+		v[i] = min(math.sqrt( v[i-1] ** 2 + 2 * config['acceleration']*(1-EPS) ), v[i])
+	for i in range(len(v) - 2, -1, -1):
+		v[i] = min(math.sqrt( v[i+1] ** 2 - 2 * config['breaking']*(1-EPS) ), v[i])
+
+def accel(v):
+	a = np.zeros(v.shape)
+	a[:-1] = (v[1:]**2 - v[:-1]**2) / 2
+	return a
+
+def opt_with_pits(config, unconstr_v, n_pits):
+	l = len(unconstr_v)
+
+	pit_locs = np.append(np.arange(0, l, l // (n_pits+1)), [l])[:n_pits+2]
+	pit_locs[-1] = l
+
+	#print(pit_locs)
+	
+	v = unconstr_v.copy() # entire track
+	v[pit_locs[1:-1]] = 0
+
+	unconstrain(config, v)
+
+	acc = accel(v)
+
+	for start, end in zip(pit_locs[:-1], pit_locs[1:]):
+		reduce(config, acc[start:end])
+
+	acc[pit_locs[1:-1] - 1] = config['breaking']
+	
+	return acc, pit_locs[1:-1]
+
+def reduce(config, acc):	
+	gas = np.sum((0.1*np.maximum(acc, 0)**2))
+	tire = np.sum((0.1*np.minimum(acc, 0)**2))
+
+	#print(acc, gas)
+	#assert(gas > 0)
+	#assert(tire > 0)
+	
+	c = min(math.sqrt(config['gas'] / gas), math.sqrt(config['tire'] / tire), 1)
+
 	acc *= c
-	acc *= (1-EPS)
-
-	instr = np.zeros((len(acc), 2))
-	instr[:,0] = acc
-
-	return instr
-
 '''
-	# Save Instructions
+exit()
 
-	OUTPUT_PATH = "soln/"
+# Optimal travel
 
-	outarr = np.zeros((len(radius), 2))
-	outarr[:,0] = acc
+speed = np.zeros(v_max.shape)
 
-	np.savetxt(OUTPUT_PATH + '/instructions.csv', outarr, delimiter=',', header='a,pit_stop')
+for i in range(1, len(speed)):
+	speed[i] = min(math.sqrt( speed[i-1] ** 2 + 2 * ACC ), v_max[i])
 
-	#np.savetxt(OUTPUT_PATH + '/car.csv', ([TIRE, GAS, HANDLE, TOP, ACC, BRK],), delimiter=',',
-	#	header='tire,gas,handling,speed,acceleration,breaking')
+for i in range(len(speed) - 2, -1, -1):
+	speed[i] = min(math.sqrt( speed[i+1] ** 2 - 2 * BRK ), speed[i])
 
-	# Plot
-	import matplotlib.pyplot as plt
+acc = (speed[1:]**2 - speed[:-1]**2) / 2
+acc = np.append(acc, [0])
 
-	n = len(radius)
-	n = 55
+assert(acc.shape == speed.shape)
 
-	plt.subplot(2, 1, 1)
+# Costs
 
-	plt.plot(v_max[:n])
-	plt.plot(speed[:n])
+gas = np.cumsum((0.1*np.maximum(acc, 0)**2))
+tire = np.cumsum((0.1*np.minimum(acc, 0)**2))
 
-	plt.plot(acc[:n], '.-')
+# Scale to save gas money
 
-	plt.subplot(2, 1, 2)
+c_gas = math.sqrt(GAS / gas[-1])
+c_tire = math.sqrt(TIRE / tire[-1])
+c = min(c_gas, c_tire, 1) * (1 - EPS)
+acc *= c
 
-	plt.plot(gas[:n])
-	plt.plot(tire[:n])
+speed[0] = 0
+for i, a in enumerate(acc[:-1]):
+	speed[i+1] = math.sqrt(speed[i]**2 + 2*a)
 
-	plt.show()
+# Compute stats
+
+time = 0
+for v, v_next, a in zip(speed[:-1], speed[1:], acc[:-1]):
+	time += ((v_next - v) / a) if abs(a) > EPS else 1 / v
+
+gas = np.cumsum((0.1*np.maximum(acc, 0)**2))
+tire = np.cumsum((0.1*np.minimum(acc, 0)**2))
+
+instr = np.zeros((len(acc), 2))
+instr[:,0] = acc
+
+return instr, time, gas, tire
+
+# Save Instructions
+
+OUTPUT_PATH = "soln/"
+
+outarr = np.zeros((len(radius), 2))
+outarr[:,0] = acc
+
+np.savetxt(OUTPUT_PATH + '/instructions.csv', outarr, delimiter=',', header='a,pit_stop')
+
+#np.savetxt(OUTPUT_PATH + '/car.csv', ([TIRE, GAS, HANDLE, TOP, ACC, BRK],), delimiter=',',
+#	header='tire,gas,handling,speed,acceleration,breaking')
+
+# Plot
+import matplotlib.pyplot as plt
+
+n = len(radius)
+n = 55
+
+plt.subplot(2, 1, 1)
+
+plt.plot(v_max[:n])
+plt.plot(speed[:n])
+
+plt.plot(acc[:n], '.-')
+
+plt.subplot(2, 1, 2)
+
+plt.plot(gas[:n])
+plt.plot(tire[:n])
+
+plt.show()
 '''
--- src/run.py
+++ src/run.py
@@ -5,24 +5,43 @@ import model
 import score
 
 TRACK_DIR = '../data/'
-tracks = ['track_{}.csv'.format(i) for i in range(1, 11)]
+track_files = ['track_{}.csv'.format(i) for i in range(1, 9)]
+tracks = [np.loadtxt(TRACK_DIR + track_file, skiprows=1) for track_file in track_files]
+tracks_w = [1, 0.25, 0.25, 0.25, 0.5, 0.5, 1, 1]
 
-for track_file in tracks:
-	track = np.loadtxt(TRACK_DIR + track_file, skiprows=1)
+SOLUTION_DIR = '../soln/'
 
-	best = {'time': float('inf')}
+best_time = float('inf')
+for i, config in enumerate(car_configs.gen()[:]):
+	all_instr = []
 
-	for config in car_configs.gen():
-		instr = model.optimize(config, track)
-		
-		stats = score.score(config, track, instr)
-		
-		if stats['time'] < best['time']:
-			best = stats
-			best_instr = instr
+	total_time = 0
+	for track, track_w in zip(tracks, tracks_w):
+		instr, time, gas, tire = model.optimize(config, track)
+		total_time += track_w * time
+		all_instr.append(instr)
 
-	print(track_file)
-	print(best)
+	if total_time < best_time:
+		best_time = total_time
+		best_config = config
+		best_instr = all_instr
+
+	print(i)
+
+	#print(config)
+	#print(total_time)
+
+print(best_config)
+print(best_time)
+print([np.sum(instr[:,1]) for instr in best_instr])
+
+for i, instr in enumerate(best_instr):
+	np.savetxt(SOLUTION_DIR + 'instructions_{}.csv'.format(i+1), instr, delimiter=',', fmt='%f',
+		header='a,pit_stop', comments='') 
+
+
+np.savetxt(SOLUTION_DIR + 'car.csv', (best_config,), delimiter=',', fmt='%d',
+	header='tire,gas,handling,speed,acceleration,breaking', comments='')
 
 exit()
 
